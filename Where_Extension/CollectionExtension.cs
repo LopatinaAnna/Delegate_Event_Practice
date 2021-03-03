f7783f69aeef0d28c53466c1355acda02614f016
Anna Lopatina <an.lopatina050@gmail.com>
2021-03-03 18:43:48 +0200

    Add collection extension

---
+++ Where_Extension/CollectionExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Where_Extension
+{
+    public static class CollectionExtension
+    {
+        public static ICollection<int> Where(this ICollection<int> collection, Func<int, bool> func)
+        {
+            var result = new List<int>();
+
+            foreach (var item in collection)
+            {
+                if (func(item))
+                {
+                    result.Add(item);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Where_Extension/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Where_Extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 1, 2, 3, 4, 5 };
+            var result = list.Where(c => c > 3);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
